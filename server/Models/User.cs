2b8022dcc61d815b7db38770bf27bbdcc6ee70bf
farrahpepino <idcfarrah@gmail.com>
2025-12-11 11:59:47 -0500

    created user model

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -1,3 +1,10 @@
-public class User{
-    
+namespace server.Models
+{
+    public class User {
+        public string? Id {get; set;} 
+        public string Username {get; set;}
+        public string Email {get; set;}
+        public string Password {get; set;}
+        public required DateTime CreatedAt {get; set;} = DateTime.Now;
+    }
 }
