9f71fd962eee00bcfe1256458648d523424b26d8
farrahpepino <idcfarrah@gmail.com>
2026-01-17 13:52:54 -0500

    Added jwt service

--- server/Services/JwtService.cs
+++ server/Services/JwtService.cs
@@ -1,5 +1,42 @@
-namespace server.Services {
-    public class JwtService {
-        
+using Microsoft.IdentityModel.Tokens; 
+using Microsoft.Extensions.Configuration; 
+using System.IdentityModel.Tokens.Jwt; 
+using System.Security.Claims; 
+using System.Text;
+
+namespace server.Services{
+    public class JwtService: IJwtService{
+        private readonly IConfiguration _configuration;
+        private readonly string? _secret;
+
+        public JwtService (IConfiguration configuration){
+            _configuration = configuration;
+            _secret = configuration["Jwt:Secret"] ?? throw new ArgumentNullException("Jwt:Secret is missing from configuration.");
+        }
+
+        public string GenerateToken(string userId, string username, string email)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username),
+                new Claim(JwtRegisteredClaimNames.Email, email), 
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebbbd42ad1ae71b5eeacad9098c8a13b4ed736e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74baf3c013ac54442935c88f33626c806968b658")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
