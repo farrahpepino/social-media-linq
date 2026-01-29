0725a38dcdbf228449e1ba67da2a5a29459eda83
farrahpepino <idcfarrah@gmail.com>
2026-01-29 15:08:45 -0500

    c

--- client/src/Components/Navbar/navbar/navbar.ts
+++ client/src/Components/Navbar/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarWeb } from '../navbar-web/navbar-web';
+import { NavbarMobile } from '../navbar-mobile/navbar-mobile';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NavbarWeb],
+  imports: [NavbarWeb, NavbarMobile],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
