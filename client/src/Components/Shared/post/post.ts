2101ef602eb96bdd45d0c5e2b1dddf6fdc0850b9
farrahpepino <idcfarrah@gmail.com>
2026-01-29 16:25:22 -0500

    added datetime

--- client/src/Components/Shared/post/post.ts
+++ client/src/Components/Shared/post/post.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './post.html',
   styleUrl: './post.css'
 })
-export class Post {
+
+export class Post implements OnInit, OnDestroy {
   showForm = false;
+  today: Date = new Date();
+  private timerId!: any;
+
+  ngOnInit() {
+    this.timerId = setInterval(() => {
+      this.today = new Date();
+    }, 60000); 
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerId);
+  }
   
   toggleForm() {
     this.showForm = !this.showForm;
