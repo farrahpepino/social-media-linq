5119c94c296eb48edeb1b0ba098a67f9f53748d1
farrahpepino <idcfarrah@gmail.com>
2026-01-29 15:30:09 -0500

    c

--- client/src/Components/Navbar/navbar-mobile/navbar-mobile.ts
+++ client/src/Components/Navbar/navbar-mobile/navbar-mobile.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Loading } from '../../Shared/loading/loading';
+import { AuthService } from '../../../Services/auth-service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-navbar-mobile',
-  imports: [],
+  imports: [CommonModule, Loading],
   templateUrl: './navbar-mobile.html',
   styleUrl: './navbar-mobile.css',
 })
 export class NavbarMobile {
+  loading = false;
+  constructor (private auth: AuthService) {}
 
+  signOut(){
+    this.loading = true;
+    this.auth.logout()
+    .pipe(
+      finalize(() => this.loading = false) 
+    );
+    
+    window.location.replace('/'); 
+  }
 }
