fd906f94c029acdf188a7a0023562615bf8f98b4
farrahpepino <idcfarrah@gmail.com>
2026-01-29 14:31:16 -0500

    c

--- client/src/Components/Shared/post/post.ts
+++ client/src/Components/Shared/post/post.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post.html',
   styleUrl: './post.css'
 })
 export class Post {
-
+  showForm = false;
+  
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+  
 }
