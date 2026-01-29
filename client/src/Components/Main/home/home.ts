75e10a16d491185f7d19ae3142f84cbac5b19fb7
farrahpepino <idcfarrah@gmail.com>
2026-01-29 14:47:21 -0500

    c

--- client/src/Components/Main/home/home.ts
+++ client/src/Components/Main/home/home.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../Navbar/navbar/navbar';
 import { Post } from '../../Shared/post/post';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Post],
+  imports: [Navbar, Post, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  menu = false;
 
+  showMenu(){
+    this.menu = !this.menu;
+  }
 }
