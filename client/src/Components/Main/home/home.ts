29870760ed2ff4b695cf2c51a09bcf9f90a52cc6
farrahpepino <idcfarrah@gmail.com>
2026-02-06 14:41:02 -0500

    c

--- client/src/Components/Main/home/home.ts
+++ client/src/Components/Main/home/home.ts
@@ -11,12 +11,12 @@ import { PostModel } from '../../../Models/PostModel';
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
-  menu = false;
   posts: PostModel[] = [];
+  selectedPost: string | null = null;
+
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-
     this.postService.getFeed("change-this").subscribe({
       next: (res)=>{
         this.posts = res;
@@ -27,12 +27,20 @@ export class Home implements OnInit {
     });
   }
 
-  showMenu(){
-    this.menu = !this.menu;
+  showDropdown(id: string){
+    this.selectedPost = this.selectedPost === id ? null : id;
   }
 
   deletePost(id: string) {
-    this.postService.deletePost(id);
+    this.postService.deletePost(id).subscribe({
+      next: ()=>{
+        this.posts = this.posts.filter(p => p.id !== id);
+        this.selectedPost = null;
+      },
+      error: (err)=> {
+        console.error("Unable to delete post: ", err);
+      }
+    });
   }
 
 }
