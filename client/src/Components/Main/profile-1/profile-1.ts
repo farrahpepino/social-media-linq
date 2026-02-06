e377ed4402e22fa20ec25271a327ed397f5a0f5b
farrahpepino <idcfarrah@gmail.com>
2026-02-06 14:54:49 -0500

    start profile

--- client/src/Components/Main/profile-1/profile-1.ts
+++ client/src/Components/Main/profile-1/profile-1.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Navbar } from '../../Navbar/navbar/navbar';
+import { Post } from '../../Shared/post/post';
+import { CommonModule } from '@angular/common';
+import { PostService } from '../../../Services/post-service';
+import { PostModel } from '../../../Models/PostModel';
 
 @Component({
   selector: 'app-profile-1',
-  imports: [],
+  imports: [Navbar, Post, CommonModule],
   templateUrl: './profile-1.html',
   styleUrl: './profile-1.css',
 })
-export class Profile1 {
+export class Profile1 implements OnInit {
+  posts: PostModel[] = [];
+  selectedPost: string | null = null;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getFeed("change-this").subscribe({
+      next: (res)=>{
+        this.posts = res;
+      },
+      error: (err) => {
+        console.error("Error fetching posts: ", err)
+      }
+    });
+  }
+
+  showDropdown(id: string){
+    this.selectedPost = this.selectedPost === id ? null : id;
+  }
+
+  deletePost(id: string) {
+    this.postService.deletePost(id).subscribe({
+      next: ()=>{
+        this.posts = this.posts.filter(p => p.id !== id);
+        this.selectedPost = null;
+      },
+      error: (err)=> {
+        console.error("Unable to delete post: ", err);
+      }
+    });
+  }
 
 }
--- client/src/Components/Main/profile-2/profile-2.ts
+++ client/src/Components/Main/profile-2/profile-2.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { Post } from '../../Shared/post/post';
+import { PostModel } from '../../../Models/PostModel';
+import { PostService } from '../../../Services/post-service';
 
 @Component({
   selector: 'app-profile-2',
-  imports: [],
+  imports: [Post],
   templateUrl: './profile-2.html',
   styleUrl: './profile-2.css',
 })
 export class Profile2 {
+  posts: PostModel[] = [];
+  selectedPost: string | null = null;
 
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getFeed("change-this").subscribe({
+      next: (res)=>{
+        this.posts = res;
+      },
+      error: (err) => {
+        console.error("Error fetching posts: ", err)
+      }
+    });
+  }
+
+  showDropdown(id: string){
+    this.selectedPost = this.selectedPost === id ? null : id;
+  }
+
+  deletePost(id: string) {
+    this.postService.deletePost(id).subscribe({
+      next: ()=>{
+        this.posts = this.posts.filter(p => p.id !== id);
+        this.selectedPost = null;
+      },
+      error: (err)=> {
+        console.error("Unable to delete post: ", err);
+      }
+    });
+  }
 }
