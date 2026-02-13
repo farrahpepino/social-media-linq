573cc617b0df93e1ea5ec9819a73ddca117a9622
farrahpepino <idcfarrah@gmail.com>
2026-02-13 06:49:51 -0500

    c

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef8b9bd7047b488740898267ed044a3459a19cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9c66a1f8fb9f084bbcddb464f5a7269c457810")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
